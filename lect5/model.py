686606efef1dabaf8ee244dc808d889b4c3efa5e
MikiKru <michal_kruczkowski@o2.pl>
2021-12-20 14:03:46 +0100

    lect4

--- lect5/model.py
+++ lect5/model.py
@@ -1,4 +1,5 @@
-from datetime import date
+from datetime import date, datetime
+
 
 # model class - determine the structure of the object
 # model class - determine the structure of the data
@@ -9,12 +10,13 @@ class TaskModel:                # camel case
         self.start_date = start_date
         self.status = status
     def __str__(self):                                                                  # str representation of the object
-        return f"task name: {self.name} \nstart: {self.start_date} \nstop: {self.stop_date} \ncurrent status: {self.status}"
+        return f"task name: {self.name} \nstart: {self.start_date.strftime('%Y-%m-%d')} " \
+            f"\nstop: {self.stop_date.strftime('%Y-%m-%d')} \ncurrent status: {self.status}"
     def get_name(self):         # snake case
         return self.name
 
-tm1 = TaskModel("Python programming", "2022-02-02")
-tm2 = TaskModel("Signal Processing", "2022-01-20", start_date="2022-01-01")
+tm1 = TaskModel("Python programming", datetime(2020, 12, 31))
+tm2 = TaskModel("Signal Processing", stop_date=datetime(2022, 10, 1), start_date=datetime(2022, 1, 1))
 print(tm1)
 print(tm2)
 
